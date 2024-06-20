1b56a16cbb259761e3bda2d9d19d0538c5c70c14
Joaquin Faundez <142858900+jfaundez07@users.noreply.github.com>
2024-06-19 22:33:28 -0400

    Add files via upload

---
+++ DDL.sql
@@ -0,0 +1,29 @@
+CREATE TABLE ESCUDERIA (
+    idEscuderia INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    pais VARCHAR(50),
+    director VARCHAR(100),
+    campeonatos INT,
+    monoplaza VARCHAR(50)
+);
+
+CREATE TABLE PILOTO (
+    numeroCarrera INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    nacionalidad VARCHAR(50),
+    fechaNac DATE,
+    poles INT,
+    victorias INT,
+    idEscuderia INT,
+    FOREIGN KEY (idEscuderia) REFERENCES ESCUDERIA(idEscuderia)
+);
+
+CREATE TABLE CIRCUITO (
+    idCircuito INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    pais VARCHAR(50),
+    ciudad VARCHAR(50),
+    longitud FLOAT,
+    vueltas INT
+);
